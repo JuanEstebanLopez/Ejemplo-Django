06cc7e8216c89ff4106ee64b4e2babe999fd0b89
juan esteban lopez garcia <jelg1994@gmail.com>
2020-09-21 21:19:16 -0500

    Create Models

--- task_app/models.py
+++ task_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+    color = models.CharField('Hex color',max_length=7)
+    icon = models.CharField('Material Design Icon code',max_length=30)
+
+    def __str__(self):
+        return str(self.id) + ': ' + self.name
+
+class Task(models.Model):
+    id = models.AutoField(primary_key=True)
+    category = models.ForeignKey(Category, related_name='tasks', on_delete=models.CASCADE)
+    title = models.CharField(max_length=30)
+    description = models.TextField(max_length=500, null=True, blank=True)
+    date = models.DateTimeField(null=True, blank=True)
+    complete = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.id) + ': ' + self.title
